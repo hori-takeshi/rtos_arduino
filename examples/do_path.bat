351021ce18cdca6a7b1fbc6ab0d3e6d878c0d603
Takeshi Hori <t-hori@sol.dti.ne.jp>
2020-05-19 18:48:35 +0900

    updated PATH and ARDUINO_DIR for arduino IDE v1.8.12.

--- examples/do_path.bat
+++ examples/do_path.bat
@@ -1,2 +1,8 @@
-SET ARDUINO_DIR=C:\Program Files (x86)\Arduino
-SET PATH=%ARDUINO_DIR%\hardware\tools\gcc-arm-none-eabi-4.8.3-2014q1\bin;%ARDUINO_DIR%\hardware\arduino\sam\system\CMSIS\Examples\cmsis_example\gcc_arm;%PATH%;
+@echo off
+SET ARDUINO_DIR=%HOMEPATH%\AppData\Local\Arduino15\packages\arduino
+
+FOR /D %%V IN (%ARDUINO_DIR%\hardware\sam\*) DO ( SET _TEMP1=%%~fV)
+FOR /D %%V IN (%ARDUINO_DIR%\tools\arm-none-eabi-gcc\*) DO ( SET _TEMP2=%%~fV)
+
+SET PATH=%_TEMP2%\bin;%_TEMP1%\system\CMSIS\Examples\cmsis_example\gcc_arm;%PATH%
+
